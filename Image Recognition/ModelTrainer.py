5cc122b593cb91aea3827ad10621801dc463a6f2
randallctc <123568912+randallctc@users.noreply.github.com>
2025-09-14 23:36:39 +0800

    Trained New Model

--- Image Recognition/ModelTrainer.py
+++ Image Recognition/ModelTrainer.py
@@ -1,6 +1,6 @@
 from ultralytics import YOLO
-model = YOLO("yolov8m.pt")
+model = YOLO("yolov8n.pt")
 model.info()
-model.train(data = r"C:\Users\Randall Chiang\Documents\GitHub\SC2079-MDP-Group-11\Image Recognition\data.yaml", epochs = 24, imgsz = 640)
+model.train(data = r"C:\Users\Randall Chiang\Documents\GitHub\SC2079-MDP-Group-11\Image Recognition\data2.yaml", epochs = 20, imgsz = 640)
 metrics = model.val()
 print(metrics)
